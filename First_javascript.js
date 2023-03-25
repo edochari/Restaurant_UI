b39ac2f0747df2f2b948135c7f6f4a8f130b0181
hari krishna <hari.jsdeveloper@gmail.com>
2023-03-25 17:14:02 +0530

    first_commit

---
+++ First_javascript.js
@@ -0,0 +1,12 @@
+var navLinks=document.getElementsByClassName('nav-links')
+const myFunction=()=>{
+  if(navLinks.display=="block")
+  {
+    navLinks.display="none";
+  }
+  else{
+    navLinks.display="block";
+  }
+  console.log("pi")
+}
+console.log("hari");
